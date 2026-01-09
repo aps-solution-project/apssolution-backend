9f59bb09aa3e75483e772f574090dd2a3b4d864e
carino2000 <carino9231@gmail.com>
2026-01-09 17:08:02 +0900

    project init

---
+++ src/main/java/org/example/apssolution/ApssolutionBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.apssolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApssolutionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApssolutionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apssolution/ApssolutionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apssolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApssolutionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
